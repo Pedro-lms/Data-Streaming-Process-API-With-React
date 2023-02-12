73f53f524f9f87ac1ee1813e2bf6e5748da66209
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-02-12 15:41:15 -0300

    Changing controller

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,83 +1,115 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Castle.Core.Smtp;
+using Fluent.Infrastructure.FluentModel;
+using Fluent.Infrastructure.FluentStartup;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Data_Streaming_Process_API_With_React.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: LoginController
-        public ActionResult Index()
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly EmailService _emailSender;
+
+        public LoginController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
         {
             return View();
         }
 
-        // GET: LoginController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: LoginController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: LoginController/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        [Route("api/[controller]/[action]")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
         {
-            try
+            if (!ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                return BadRequest(ModelState);
             }
-            catch
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
             {
-                return View();
+                return Ok();
+            }
+            if (result.IsLockedOut)
+            {
+                return StatusCode(StatusCodes.Status429TooManyRequests);
+            }
+            else
+            {
+                return BadRequest();
             }
         }
 
-        // GET: LoginController/Edit/5
-        public ActionResult Edit(int id)
+        [HttpGet]
+        public IActionResult Register()
         {
             return View();
         }
 
-        // POST: LoginController/Edit/5
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                var user = new IdentityUser { UserName = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
             }
-            catch
-            {
-                return View();
-            }
+            return View(model);
         }
 
-        // GET: LoginController/Delete/5
-        public ActionResult Delete(int id)
+        [HttpGet]
+        public IActionResult ForgotPassword()
         {
             return View();
         }
 
-        // POST: LoginController/Delete/5
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+                {
+                    // Don't reveal that the user does not exist or is not confirmed
+                    return RedirectToAction("ForgotPasswordConfirmation");
+                }
+                // For more information on how to enable account confirmation and password reset please 
+                // visit https://go.microsoft.com/fwlink/?LinkID=532713
+                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                var callbackUrl = Url.RouteUrl(user.Id, code, Request.Scheme);
+                //await _emailSender.SendAsync(model.Email + "Reset Password" +
+                //   $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
+                return RedirectToAction("ForgotPasswordConfirmation");
             }
-            catch
-            {
-                return View();
-            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
         }
+
+  
     }
 }
